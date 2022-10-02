070721b8fee0c74baad29f3dcb81a5ac43058513
zifgu <gfzting@gmail.com>
2022-10-01 18:04:10 -0700

    Fix bugs in amenities list

--- client/src/AmenitiesList.js
+++ client/src/AmenitiesList.js
@@ -80,8 +80,8 @@ class AmenitiesList extends Component {
                         onChange={(evt) => this.setState({availability: evt.target.value})}
                     >
                         <option value={""}>- Availability -</option>
-                        <option value={"available"}>Full</option>
-                        <option value={"unavailable"}>Not yet full</option>
+                        <option value={"available"}>Not yet full</option>
+                        <option value={"unavailable"}>Full</option>
                     </select>
                     <input
                         className="form-control"
@@ -173,11 +173,11 @@ class Amenity extends Component {
                                 </h5>
                                 {
                                     here ?
-                                        <button className="btn btn-sm btn-danger" onClick={() => this.onLogout(amenity)}>Tap out
+                                        <button className="btn btn-danger" onClick={() => this.onLogout(amenity)}>Tap out
                                             <i className="bi bi-box-arrow-right ms-1"></i>
                                         </button> :
                                         canTapIn ?
-                                            <button className="btn btn-sm btn-primary" onClick={() => this.onLogin(amenity)}>Tap in
+                                            <button className="btn btn-primary" onClick={() => this.onLogin(amenity)}>Tap in
                                                 <i className="bi bi-box-arrow-in-right ms-1"></i>
                                             </button> :
                                             null
